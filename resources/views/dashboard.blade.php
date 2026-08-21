3741ed570744f18c0fa8729b4d9e1ed09553bba6
Oyatillo2002 <xoyatillo921@gmail.com>
2026-08-21 19:43:28 +0500

    Manager and Client's dashboards done!

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,9 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="p-6 text-gray-900 dark:text-green-500">
                     @if (auth()->user()->role->name == 'manager')
-                        {{ __("Received Applications") }}
+                        {{ __('Received Applications') }}
 
                         <div class='mt-5'>
                             <div class="rounded-xl border p-5 mt-5 shadow-md w-9/12 bg-white">
@@ -23,13 +23,14 @@
                                     </div>
                                     <div class="flex items-center space-x-8">
                                         <button
-                                            class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">4543</button>
+                                            class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs text-neutral-500 font-semibold">4543</button>
                                         <div class="text-xs text-neutral-500">2 hours ago</div>
                                     </div>
                                 </div>
 
                                 <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
+                                    <div class="mb-3 text-blue-500 font-bold">Nulla sed leo tempus, feugiat velit vel,
+                                        rhoncus
                                         neque?</div>
                                     <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna.
                                         Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
@@ -57,13 +58,14 @@
                                     </div>
                                     <div class="flex items-center space-x-8">
                                         <button
-                                            class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">4543</button>
+                                            class="rounded-2xl border bg-100 px-3 py-1 text-xs text-neutral-500  font-semibold">4543</button>
                                         <div class="text-xs text-neutral-500">2 hours ago</div>
                                     </div>
                                 </div>
 
                                 <div class="mt-4 mb-6">
-                                    <div class="mb-3 text-xl font-bold">Nulla sed leo tempus, feugiat velit vel, rhoncus
+                                    <div class="mb-3 text-blue-500 font-bold">Nulla sed leo tempus, feugiat velit vel,
+                                        rhoncus
                                         neque?</div>
                                     <div class="text-sm text-neutral-600">Aliquam a tristique sapien, nec bibendum urna.
                                         Maecenas convallis dignissim turpis, non suscipit mauris interdum at. Morbi sed
@@ -82,7 +84,32 @@
                             </div>
                         </div>
                     @else
-                        {{ __("You're Client!") }}
+                        {{-- {{ __("Your client") }} --}}
+                        <div
+                            class='flex items-center  from-teal-100 via-teal-300 to-teal-500 bg-white-to-br'>
+                            <div class='w-full max-w-lg px-10 py-8 mx-auto  rounded-lg shadow-xl'>
+                                <div class='max-w-md mx-auto space-y-6'>
+                                    <form action="" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <h2 class="text-2xl font-bold ">Submit your application</h2>
+                                        <hr class="my-6">
+                                        <label class="uppercase text-sm font-bold opacity-70">Subject</label>
+                                        <input type="text"
+                                            class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <label class="uppercase text-sm font-bold opacity-70">Message</label>
+                                        <textarea 
+                                         rows="5" class=" p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none"></textarea>
+                                        <label class="uppercase text-sm font-bold opacity-70">file</label>
+                                        <input type="file"
+                                            class="p-3 mt-2 mb-4 w-full bg-slate-200 rounded border-2 border-slate-200 focus:border-slate-600 focus:outline-none">
+                                        <input type="submit"
+                                            class="py-3 px-6 my-2 bg-emerald-500 text-white font-medium rounded hover:bg-indigo-500 cursor-pointer ease-in-out duration-300"
+                                            value="Send">
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
                     @endif
                 </div>
             </div>
